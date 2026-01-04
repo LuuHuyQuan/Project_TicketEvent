b88de7d9d06cbfa077969cce5beb0852e7794fbd
Lưu Huy Quân <144649362+LuuHuyQuan@users.noreply.github.com>
2026-01-04 13:41:43 +0700

    Update PasswordHasher.cs

--- Project_ApiTicketEvent/Services/Security/PasswordHasher.cs
+++ Project_ApiTicketEvent/Services/Security/PasswordHasher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace Services.Security
 {
-    internal class PasswordHasher
+    public static class PasswordHasher
     {
+        private static readonly PasswordHasher<string> _hasher
+            = new();
+
+        public static string Hash(string password)
+        {
+            return _hasher.HashPassword("user", password);
+        }
+
+        public static bool Verify(string password, string hashedPasswordFromDb)
+        {
+            var result = _hasher.VerifyHashedPassword("user", hashedPasswordFromDb, password);
+
+            return result == PasswordVerificationResult.Success
+                   || result == PasswordVerificationResult.SuccessRehashNeeded;
+        }
     }
 }
