617534a632d0129091b9d552c18cb91a4d1c8643
Lưu Huy Quân <144649362+LuuHuyQuan@users.noreply.github.com>
2026-01-04 10:48:33 +0700

    Update SqlConnectionFactory.cs

--- Project_ApiTicketEvent/Data/SqlConnectionFactory.cs
+++ Project_ApiTicketEvent/Data/SqlConnectionFactory.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    internal class SqlConnectionFactory_cs
+   public interface ISqlConnectionFactory
     {
+        SqlConnection Create();
+    }
+    public class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string _cs;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _cs = configuration.GetConnectionString("Default")
+             ?? throw new InvalidOperationException("Missing ConnectionStrings:Default");
+        }
+        public SqlConnection Create()
+        {
+            return new SqlConnection(_cs);
+        }
     }
 }
