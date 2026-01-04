5702486d822245859f30cb593d4f2aeda0e064b4
Tatuan2909 <tatuananh29092005@gmail.com>
2026-01-04 22:13:23 +0700

    them LoaiVeRepository

--- Project_ApiTicketEvent/Repositories/Interfaces/ILoaiVeRepository.cs
+++ Project_ApiTicketEvent/Repositories/Interfaces/ILoaiVeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Repositories.Interfaces
 {
-    internal interface ILoaiVeRepository
+    public interface ILoaiVeRepository
     {
+        Task<IEnumerable<LoaiVe>> GetAllAsync();
+        Task<LoaiVe?> GetByIdAsync(int id);
+        Task<IEnumerable<LoaiVe>> GetBySuKienAsync(int suKienId);
+        Task<int> CreateAsync(LoaiVe loaiVe);
+        Task<bool> UpdateAsync(LoaiVe loaiVe);
     }
 }
