bb6c30a8b21be6e3f518147aab48d8ac89c185fc
Lưu Huy Quân <144649362+LuuHuyQuan@users.noreply.github.com>
2026-01-04 12:09:36 +0700

    Change NguoiDung class to public and add properties

--- Project_ApiTicketEvent/Models/NguoiDung.cs
+++ Project_ApiTicketEvent/Models/NguoiDung.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class NguoiDung
+    public class NguoiDung
     {
+        public int NguoiDungId { get; set; }
+        public string HoTen { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MatKhauHash { get; set; } = string.Empty;
+
+        public int? VaiTroId { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public bool? TrangThai { get; set; }
+
+        public string TenDangNhap { get; set; } = string.Empty;
+        public string? SoDienThoai { get; set; }
     }
 }
