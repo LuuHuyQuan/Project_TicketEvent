fe0d5cff87eb4f9e963eea65a32607e62e9c1fb5
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2026-01-04 15:56:34 +0700

    Create DiaDiemAll

--- Project_ApiTicketEvent/Repositories/Implementations/DiaDiemReponsitory.cs
+++ Project_ApiTicketEvent/Repositories/Implementations/DiaDiemReponsitory.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Data;
+using Models;
+using Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repositories.Implementations
 {
-    internal class DiaDiemReponsitory
+    public class DiaDiemReponsitory : IDiaDiemReponsitory
     {
+        private readonly IDbConnectionFactory _connectionFactory;
+        public DiaDiemReponsitory(IDbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+        public int Create(DiaDiem entity)
+        {
+                using var conn = _connectionFactory.CreateConnection();
+                conn.Open();
+
+                using var cmd = conn.CreateCommand();
+                cmd.CommandText = @"
+                INSERT INTO dbo.DiaDiem (TenDiaDiem, DiaChi, SucChua, MoTa, TrangThai)
+                OUTPUT INSERTED.DiaDiemID
+                VALUES (@TenDiaDiem, @DiaChi, @SucChua, @MoTa, @TrangThai);";
+
+                AddParam(cmd, "@TenDiaDiem", entity.TenDiaDiem);
+                AddParam(cmd, "@DiaChi", entity.DiaChi);
+                AddParam(cmd, "@SucChua", entity.SucChua);
+                AddParam(cmd, "@MoTa", entity.MoTa);
+                AddParam(cmd, "@TrangThai", entity.TrangThai);
+
+                var obj = cmd.ExecuteScalar();
+                return Convert.ToInt32(obj);
+        }
+
+        public bool Delete(int id)
+        {
+            using var conn = _connectionFactory.CreateConnection();
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = @"UPDATE dbo.DiaDiem SET TrangThai = 0 WHERE DiaDiemID = @Id;";
+            AddParam(cmd, "@Id", id);
+            return cmd.ExecuteNonQuery() > 0;
+        }
+
+        public bool Update(DiaDiem entity)
+        {
+            using var conn = _connectionFactory.CreateConnection();
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = @"
+            UPDATE dbo.DiaDiem
+            SET TenDiaDiem = @TenDiaDiem,
+                DiaChi     = @DiaChi,
+                SucChua    = @SucChua,
+                MoTa       = @MoTa,
+                TrangThai  = @TrangThai
+            WHERE DiaDiemID = @Id;";
+
+            AddParam(cmd, "@Id", entity.DiaDiemID);
+            AddParam(cmd, "@TenDiaDiem", entity.TenDiaDiem);
+            AddParam(cmd, "@DiaChi", entity.DiaChi);
+            AddParam(cmd, "@SucChua", entity.SucChua);
+            AddParam(cmd, "@MoTa", entity.MoTa);
+            AddParam(cmd, "@TrangThai", entity.TrangThai);
+
+            return cmd.ExecuteNonQuery() > 0;
+        }
+        private static void AddParam(IDbCommand cmd, string name, object? value)
+        {
+            var p = cmd.CreateParameter();
+            p.ParameterName = name;
+            p.Value = value ?? DBNull.Value;
+            cmd.Parameters.Add(p);
+        }
     }
 }
---
+++ Project_ApiTicketEvent/Services/Interfaces/IDiaDiemService.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IDiaDiemService
+    {
+        int Create(DiaDiem entity);
+        bool Update(DiaDiem entity);
+        bool Delete(int id);
+    }
+}
