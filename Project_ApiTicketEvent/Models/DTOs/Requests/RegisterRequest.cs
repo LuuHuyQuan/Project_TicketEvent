1f722f1bfb63194b7bbcd168b6303ca92306c7e3
Lưu Huy Quân <144649362+LuuHuyQuan@users.noreply.github.com>
2026-01-04 12:32:08 +0700

    Change RegisterRequest class to public

--- Project_ApiTicketEvent/Models/DTOs/Requests/RegisterRequest.cs
+++ Project_ApiTicketEvent/Models/DTOs/Requests/RegisterRequest.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Models.DTOs.Requests
 {
-    internal class RegisterRequest
+    public class RegisterRequest
     {
+        public string HoTen { get; set; } = string.Empty;
+        public string TenDangNhap { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MatKhau { get; set; } = string.Empty;
+        public string? SoDienThoai { get; set; }
     }
 }
