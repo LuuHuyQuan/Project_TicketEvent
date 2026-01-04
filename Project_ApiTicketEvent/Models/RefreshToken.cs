894b2613cfede2b16d742a737e8a84099248a79c
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2026-01-04 12:07:36 +0700

    Create Models Login

--- Project_ApiTicketEvent/Models/RefreshToken.cs
+++ Project_ApiTicketEvent/Models/RefreshToken.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories.Interfaces
+namespace Models
 {
-    internal class Class1
+    internal class RefreshToken
     {
     }
 }
