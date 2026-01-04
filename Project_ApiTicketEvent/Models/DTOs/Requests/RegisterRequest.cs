c0a2ad70a4658f52afcca0c0c348bf213aa18018
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2026-01-04 14:30:52 +0700

    Successfully ApiLogin

--- Project_ApiTicketEvent/Models/DTOs/Requests/RegisterRequest.cs
+++ Project_ApiTicketEvent/Models/DTOs/Requests/RegisterRequest.cs
@@ -12,6 +12,7 @@ namespace Models.DTOs.Requests
         public string TenDangNhap { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string MatKhau { get; set; } = string.Empty;
+        public int VaiTroId { get; set; }
         public string? SoDienThoai { get; set; }
     }
 }
