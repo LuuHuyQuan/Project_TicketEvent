bc68015c51793aab6119e8b0b646d613101e56a3
Lưu Huy Quân <144649362+LuuHuyQuan@users.noreply.github.com>
2026-01-04 12:09:12 +0700

    Change RefreshToken class to public and add properties

--- Project_ApiTicketEvent/Models/RefreshToken.cs
+++ Project_ApiTicketEvent/Models/RefreshToken.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class RefreshToken
+    public class RefreshToken
     {
+        public int RefreshTokenId { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public string JwtId { get; set; } = string.Empty;
+
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RevokedAt { get; set; }
+
+        public bool IsRevoked { get; set; }
+        public bool IsUsed { get; set; }
     }
 }
