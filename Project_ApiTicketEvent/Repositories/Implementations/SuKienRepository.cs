7edffa35f8f4736ead75e552db4d6627234b439d
Tatuan2909 <tatuananh29092005@gmail.com>
2026-01-04 20:20:45 +0700

    update SuKienSuKienRepository

--- Project_ApiTicketEvent/Repositories/Implementations/SuKienRepository.cs
+++ Project_ApiTicketEvent/Repositories/Implementations/SuKienRepository.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using Data;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using Repositories.Interfaces;
 
 namespace Repositories.Implementations
 {
-    internal class SuKienRepository
+    public class SuKienRepository : ISuKienRepository
     {
+        private readonly IDbConnectionFactory _connectionFactory;
+
+        public SuKienRepository(IDbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<IEnumerable<SuKien>> GetAllAsync()
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var sql = @"SELECT SuKienID, DanhMucID, DiaDiemID, ToChucID, TenSuKien, 
+                              MoTa, ThoiGianBatDau, ThoiGianKetThuc, AnhBiaUrl, 
+                              TrangThai, NgayTao 
+                       FROM SuKien 
+                       ORDER BY NgayTao DESC";
+
+            return await connection.QueryAsync<SuKien>(sql);
+        }
+
+        public async Task<SuKien?> GetByIdAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var sql = @"SELECT SuKienID, DanhMucID, DiaDiemID, ToChucID, TenSuKien, 
+                              MoTa, ThoiGianBatDau, ThoiGianKetThuc, AnhBiaUrl, 
+                              TrangThai, NgayTao 
+                       FROM SuKien 
+                       WHERE SuKienID = @Id";
+
+            return await connection.QueryFirstOrDefaultAsync<SuKien>(sql, new { Id = id });
+        }
+
+        public async Task<int> CreateAsync(SuKien suKien)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var sql = @"INSERT INTO SuKien (DanhMucID, DiaDiemID, ToChucID, TenSuKien, 
+                                           MoTa, ThoiGianBatDau, ThoiGianKetThuc, 
+                                           AnhBiaUrl, TrangThai, NgayTao)
+                       VALUES (@DanhMucID, @DiaDiemID, @ToChucID, @TenSuKien, 
+                               @MoTa, @ThoiGianBatDau, @ThoiGianKetThuc, 
+                               @AnhBiaUrl, @TrangThai, @NgayTao);
+                       SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            return await connection.QuerySingleAsync<int>(sql, suKien);
+        }
+
+        public async Task<bool> UpdateAsync(SuKien suKien)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var sql = @"UPDATE SuKien 
+                       SET DanhMucID = @DanhMucID,
+                           DiaDiemID = @DiaDiemID,
+                           ToChucID = @ToChucID,
+                           TenSuKien = @TenSuKien,
+                           MoTa = @MoTa,
+                           ThoiGianBatDau = @ThoiGianBatDau,
+                           ThoiGianKetThuc = @ThoiGianKetThuc,
+                           AnhBiaUrl = @AnhBiaUrl,
+                           TrangThai = @TrangThai
+                       WHERE SuKienID = @SuKienID";
+
+            var rowsAffected = await connection.ExecuteAsync(sql, suKien);
+            return rowsAffected > 0;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            var sql = "DELETE FROM SuKien WHERE SuKienID = @Id";
+
+            var rowsAffected = await connection.ExecuteAsync(sql, new { Id = id });
+            return rowsAffected > 0;
+        }
     }
 }
