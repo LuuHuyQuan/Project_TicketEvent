8b375b5f3254dc60ee54f2d23598cabcd8592182
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-04 15:53:25 +0700

    Update

--- Project_ApiTicketEvent/Services/Interfaces/IDanhMucSuKienService.cs
+++ Project_ApiTicketEvent/Services/Interfaces/IDanhMucSuKienService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Models;
 namespace Services.Interfaces
 {
-    internal interface IDanhMucSuKienService
+    public interface IDanhMucSuKienService
     {
+        Task<List<DanhMucSuKien>> GetAllAsync();
+        Task<DanhMucSuKien?> GetByNameAsync(string tenDanhMuc);
+
     }
 }
