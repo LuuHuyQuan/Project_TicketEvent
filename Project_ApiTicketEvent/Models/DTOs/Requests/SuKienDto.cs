1f474eb7f4fb65657cfcbf71ea2c8339ee565a96
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-04 22:04:58 +0700

    Request Sukien

---
+++ Project_ApiTicketEvent/Models/DTOs/Requests/SuKienDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Requests
+{
+    public class SuKienDto
+    {
+        public int SuKienID { get; set; }
+        public string TenSuKien { get; set; } = string.Empty;
+
+        public int DanhMucID { get; set; }
+        public string TenDanhMuc { get; set; } = string.Empty;
+
+        public int? DiaDiemID { get; set; }
+        public string? TenDiaDiem { get; set; }
+
+        public DateTime? ThoiGianBatDau { get; set; }
+        public DateTime? ThoiGianKetThuc { get; set; }
+
+        public string? MoTa { get; set; }
+        public string? AnhBia { get; set; }
+
+        public bool TrangThai { get; set; }
+    }
+}
