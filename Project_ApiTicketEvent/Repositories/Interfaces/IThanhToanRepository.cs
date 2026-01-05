f10464e90a37a7a73b5c57bbbc302cfedf732c7b
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-05 12:18:49 +0700

    tao Ithanhtoan

---
+++ Project_ApiTicketEvent/Repositories/Interfaces/IThanhToanRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using Models.DTOs.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Models.DTOs.Requests.ThanhToanRequest;
+
+namespace Repositories.Interfaces
+{
+    public interface IThanhToanRepository
+    {
+        Task<List<ThanhToan>> GetByDonHangAsync(int nguoiMuaId, int donHangId);
+        Task<int> TaoThanhToanAsync(TaoThanhToanRequest req);                 // return ThanhToanID
+        Task<object> XacNhanThanhToanAsync(XacNhanThanhToanRequest req);      // trả kết quả + vé
+    }
+}
