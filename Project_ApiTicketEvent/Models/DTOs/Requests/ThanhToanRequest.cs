1cfab2b7dc4c794475349d1518b8e2de89b6fbfd
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-05 12:16:48 +0700

    tao models thanh toan va thanhtoanrequest

---
+++ Project_ApiTicketEvent/Models/DTOs/Requests/ThanhToanRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTOs.Requests
+{
+    public class ThanhToanRequest
+    {
+        public class TaoThanhToanRequest
+        {
+            public int NguoiMuaID { get; set; }
+            public int DonHangID { get; set; }
+            public string PhuongThuc { get; set; } = "VNPAY";
+            public string? MaGiaoDich { get; set; }
+            public string? RawResponse { get; set; }
+        }
+
+        public class XacNhanThanhToanRequest
+        {
+            public int NguoiMuaID { get; set; }
+            public int DonHangID { get; set; }
+            public string PhuongThuc { get; set; } = "VNPAY";
+            public string? MaGiaoDich { get; set; }
+            public bool ThanhCong { get; set; } = true;   // true: success, false: failed
+            public string? RawResponse { get; set; }
+        }
+    }
+}
---
+++ Project_ApiTicketEvent/Models/ThanhToan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ThanhToan
+    {
+        public int ThanhToanID { get; set; }
+        public int DonHangID { get; set; }
+        public string? MaGiaoDich { get; set; }
+        public string PhuongThuc { get; set; } = string.Empty; // VNPAY, MOMO, CASH
+        public decimal SoTien { get; set; }
+        public byte TrangThai { get; set; } // TINYINT: 0,1,2,3
+        public DateTime? ThoiGianThanhToan { get; set; }
+        public string? RawResponse { get; set; }
+    }
+}
