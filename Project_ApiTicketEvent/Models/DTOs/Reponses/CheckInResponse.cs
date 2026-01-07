11c5ead4d6736eed13830f780a23f8803caa9308
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-07 20:58:53 +0700

    them checkinresponse

--- Project_ApiTicketEvent/Models/DTOs/Reponses/CheckInResponse.cs
+++ Project_ApiTicketEvent/Models/DTOs/Reponses/CheckInResponse.cs
@@ -8,6 +8,19 @@ namespace Models.DTOs.Reponses
 {
     public class CheckInResponse
     {
+        public int VeID { get; set; }
+        public string MaVe { get; set; } = string.Empty;
+        public string QrToken { get; set; } = string.Empty;
 
+        public int SuKienID { get; set; }
+        public string TenSuKien { get; set; } = string.Empty;
+
+        public int LoaiVeID { get; set; }
+        public string TenLoaiVe { get; set; } = string.Empty;
+
+        public byte TrangThaiTruoc { get; set; }
+        public byte TrangThaiSau { get; set; }
+        public DateTime ThoiGianCheckIn { get; set; }
+        public int BanToChucID { get; set; }
     }
 }
