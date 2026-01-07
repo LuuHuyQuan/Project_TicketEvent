37b20753bda829815005c8f9c40150eccfee35a1
DucToan Nguyen <ductoan.hy0111@gmail.com>
2026-01-07 21:00:52 +0700

    them Interfaces

---
+++ Project_ApiTicketEvent/Repositories/Interfaces/ICheckInRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models.DTOs.Reponses;
+using Models.DTOs.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interfaces
+{
+    public interface ICheckInRepository
+    {
+        CheckInResponse? Scan(CheckInRequest req, int banToChucId, int? suKienId = null);
+    }
+}
